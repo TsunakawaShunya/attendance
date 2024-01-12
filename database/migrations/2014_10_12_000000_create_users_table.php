40ac8175d1eaa3e777a4beb4e1e3aa7fd8771ebf
TsunakawaShunya <uu.tsunakawa@gmail.com>
2024-01-13 00:39:24 +0900

    dbからシーディングまで

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('hourly_wage');     // 時給
             $table->timestamps();
         });
     }
---
+++ database/seeders/WorkSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class WorkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userId = 1;
+        $date = now()->subDays(30); // 30日前から
+
+        for ($i = 0; $i < 30; $i++) {
+            $workStart = $date->copy()->setHours(rand(8, 11))->setMinutes(rand(0, 59));
+            $workEnd = $date->copy()->setHours(rand(16, 18))->setMinutes(rand(0, 59));
+            $breakStart = $date->copy()->setHours(rand(13, 15))->setMinutes(rand(0, 59));
+            $breakEnd = $breakStart->copy()->addMinutes(rand(30, 60));
+
+            DB::table('works')->insert([
+                'user_id' => $userId,
+                'work_start' => $workStart,
+                'work_end' => $workEnd,
+                'break_start' => $breakStart,
+                'break_end' => $breakEnd,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
+            // dateをインクリメント
+            $date->addDay();
+        }
+    }
+}
